182a9b312f6784c544731525e1d052b14c6bd333
Ares <diego.amachiflores@gmail.com>
2023-03-17 16:02:14 -0500

    04

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import './App.css'
 import Cards from './components/Cards/Cards.jsx'
 import NavBar from './components/NavBar/NavBar'
 import { useState } from 'react'
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation, MemoryRouter, useNavigate } from 'react-router-dom';
 import About from "./components/About/About.jsx";
 import Detail from "./components/Detail/Detail.jsx";
 import Form from './components/Forms/Forms'; 
 function App () {
   const [characters, setCharacters] = useState([]);
-  
+  let {pathname} = useLocation();
+
+
   function onSearch(character) {
     fetch(`https://rickandmortyapi.com/api/character/${character}`)
      .then((response) => response.json())
      .then((data) => {
         if (data.name) {
            setCharacters((oldChars) => [...oldChars, data]);
+           console.log(pathname)
         } else {
            window.alert('No hay personajes con ese ID');
         }
@@ -27,15 +30,15 @@ function App () {
 
   return (
     <div className='App' style={{ padding: '25px' }}>
-      { <Form/>? <NavBar onSearch={onSearch} />:""}
-      <hr />
+     {pathname==='/'? "" :<NavBar onSearch={onSearch}/>}
+     {pathname==='/'? "" : <hr />}
       <Routes>
         <Route path = "/about" element={<About/>} />
         <Route path = "/home" element={
           <Cards characters={characters} onClose={onClose} />
         } />
         <Route path='/detail/:detailID' element={<Detail/>} />
-        <Route path='/form' element={<Form/>}/>
+        <Route path='/' element={<Form/>}/>
       </Routes>
     </div>
   )
--- src/components/Forms/Forms.jsx
+++ src/components/Forms/Forms.jsx
@@ -1,12 +1,42 @@
+import React from "react"
+import styles from './Forms.module.css'
+
 export default function Form(props) {
+    const [userData ,  setUserData]= React.useState({
+        username :"",
+        password :"",
+    }) 
+    const handleUsernChange =e=>{
+        const {value}= e.target
+        console.log(value)
+        setUserData({
+            ...userData,
+            username :value      
+        })
+    }
+    const handlePasswChange= e =>{
+        const {value} = e.target;
+        setUserData({
+            ...userData,
+            password : value
+        })
+    }
     return (
-            
+        <div>
             <form>
                 <label >Username</label>
-                <input/>
+                <input
+                type="text"
+                onChange={handleUsernChange}
+                />
                 <label >Password</label>
-                <input type="text" />
+                <input 
+                type="text" 
+                onChange={handlePasswChange}
+                />
                 <button>login</button>
             </form> 
+        </div> 
+            
     )
 }
